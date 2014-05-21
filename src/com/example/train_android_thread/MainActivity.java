1dc11d943307824953da5a2d24d712578412c083
Thai <zzmrthaizz@gmail.com>
2014-05-21 18:15:38 +0700

    unsynchroniztion

--- src/com/example/train_android_thread/MainActivity.java
+++ src/com/example/train_android_thread/MainActivity.java
@@ -14,10 +14,13 @@ import android.widget.Toast;
 public class MainActivity extends Activity {
 	ProgressBar progressBar;
 	TextView textView;
+	ProgressBar progressBar1;
+	TextView textView1;
 	Button button;
 	Handler handler = new Handler();
 	Integer progress = 0;
 	Thread thread;
+	Thread thread1;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +28,8 @@ public class MainActivity extends Activity {
 		setContentView(R.layout.activity_main);
 		progressBar = (ProgressBar) findViewById(R.id.progress);
 		textView = (TextView) findViewById(R.id.tv_test);
+		progressBar1 = (ProgressBar) findViewById(R.id.progress1);
+		textView1 = (TextView) findViewById(R.id.tv_test1);
 		button = (Button) findViewById(R.id.btn_start);
 		thread = new Thread(new Runnable() {
 
@@ -50,11 +55,36 @@ public class MainActivity extends Activity {
 				}
 			}
 		});
+		thread1 = new Thread(new Runnable() {
+
+			@Override
+			public void run() {
+				for (int i = 0; i < 100; i++) {
+					progress += 1;
+					handler.post(new Runnable() {
+
+						@Override
+						public void run() {
+							textView1.setText(progress.toString());
+							progressBar1.setProgress(progress);
+						}
+					});
+					try {
+						Thread.sleep(1000);
+
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+			}
+		});
 		button.setOnClickListener(new OnClickListener() {
 
 			@Override
 			public void onClick(View v) {
 				thread.start();
+				thread1.start();
 
 			}
 		});
